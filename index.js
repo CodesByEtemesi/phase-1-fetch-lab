04a2ad53e9c82a76fa1a8939a0d41fb75c4b8879
ETEMESI <samweletemesi2@Gmail.com>
2024-01-08 12:37:15 +0300

    Reviewed the assignment

--- index.js
+++ index.js
@@ -1,43 +1,18 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  fetch("https://anapioficeandfire.com/api/books")
+  return fetch("https://anapioficeandfire.com/api/books")
   .then((resp) => resp.json())
-  .then((json) => {
-    //  console.log(json);
-     let cardContainer = document.getElementById("card-container");
-
-     json.forEach((item) => {
-         cardContainer.innerHTML +=  `
-         <div id="card-body">
-         <img src ="${item.url}" alt ="${item.name}" height="300px" />
- 
-         <div id= "card-info">
-         <p>${item.name}</p>
-         <p>${item.pulisher}</p>
- 
-         </div>
-         </div>
-         `;        
-         
-     });
- })
-}
-  
-  
-//   console.log(json));
-  
-// }
+  .then((data) => renderBooks(data));
+ }
 
 function renderBooks(books) {
   const main = document.querySelector('main');
-  books.forEach(book => {
+  books.forEach((book) => {
     const h2 = document.createElement('h2');
     h2.innerHTML = book.name;
     main.appendChild(h2);
   });
 }
-
-
 
 document.addEventListener('DOMContentLoaded', function() {
   fetchBooks();
