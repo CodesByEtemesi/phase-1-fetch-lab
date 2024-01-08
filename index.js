5e2a92eb79d6a1e5c125da0490f731680965b0ec
ETEMESI <samweletemesi2@Gmail.com>
2024-01-08 11:40:26 +0300

    Completed Lab

--- index.js
+++ index.js
@@ -1,7 +1,32 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  
+  fetch("https://anapioficeandfire.com/api/books")
+  .then((resp) => resp.json())
+  .then((json) => {
+    //  console.log(json);
+     let cardContainer = document.getElementById("card-container");
+
+     json.forEach((item) => {
+         cardContainer.innerHTML +=  `
+         <div id="card-body">
+         <img src ="${item.url}" alt ="${item.name}" height="300px" />
+ 
+         <div id= "card-info">
+         <p>${item.name}</p>
+         <p>${item.pulisher}</p>
+ 
+         </div>
+         </div>
+         `;        
+         
+     });
+ })
 }
+  
+  
+//   console.log(json));
+  
+// }
 
 function renderBooks(books) {
   const main = document.querySelector('main');
@@ -11,6 +36,8 @@ function renderBooks(books) {
     main.appendChild(h2);
   });
 }
+
+
 
 document.addEventListener('DOMContentLoaded', function() {
   fetchBooks();
